ffd713490bab2c675b9346fbcd1060e8da218e0c
bohdandr <106035854+bohdandr@users.noreply.github.com>
2022-10-24 12:52:59 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from itertools import product
-student_id = 1
-print(list(product(
-      ('python 3.8.*', 'python 3.7.*', 'python 3.6.*'),
-      ('venv + requirements.txt', 'virtualenv + requirements.txt', 'poetry', 'pipenv')
- ))[(student_id - 1) % 12])
